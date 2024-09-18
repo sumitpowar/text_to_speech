229cf207fe456f4ca53d2850b4844d98ef0b7a88
sumitpowar <61697486+sumitpowar@users.noreply.github.com>
2024-09-18 13:00:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,8 @@ import base64
 def text_to_speech(text):
   audio = gTTS(text, lang='en')
   speech_bytes = io.BytesIO()
-  audio.write_to_fp(speech_bytes)
-  speech_bytes.seek(0)
-
-  # Convert speech to base64 encoding
-  speech_base64 = base64.b64encode(speech_bytes.read()).decode('utf-8')
-
-  return speech_base64
+  
+  return audio.write_to_fp(speech_bytes)
 
 
 def main():
@@ -28,10 +23,10 @@ def main():
     audio_bytes = text_to_speech(user_input)
     
     st.audio(audio_bytes, format='audio/mp3')
-    st.download_button(label="Download Speech",
-                       data=audio_bytes,
-                       file_name="speech.mp3",
-                       mime="audio/mp3")
+    # st.download_button(label="Download Speech",
+    #                    data=audio_bytes,
+    #                    file_name="speech.mp3",
+    #                    mime="audio/mp3")
 
 if __name__ == "__main__":
   main()
