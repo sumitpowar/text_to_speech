aefc4cba740c7529757e3829abfed3c253b6a41e
sumitpowar <61697486+sumitpowar@users.noreply.github.com>
2024-09-18 12:45:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,35 @@
 import streamlit as st
+from gtts import gTTS
 
-st.title('SEBI Library Podcast')
+def text_to_speech(text):
+  audio = gTTS(text, lang='en')
+  speech_bytes = io.BytesIO()
+  audio.write_to_fp(speech_bytes)
+  speech_bytes.seek(0)
 
-st.write('Hello world!')
+  # Convert speech to base64 encoding
+  speech_base64 = base64.b64encode(speech_bytes.read()).decode('utf-8')
+
+  return speech_base64
+
+
+def main():
+  st.title("Library Podcast")
+
+  # Text input
+  user_input = st.text_area("Enter the text you want to convert to speech:",
+                            height=150)
+
+  
+  # Convert button
+  if st.button("Convert to Speech"):
+    audio_bytes = text_to_speech(user_input)
+    
+    st.audio(audio_bytes, format='audio/mp3')
+    st.download_button(label="Download Speech",
+                       data=audio_bytes,
+                       file_name="speech.mp3",
+                       mime="audio/mp3")
+
+if __name__ == "__main__":
+  main()
